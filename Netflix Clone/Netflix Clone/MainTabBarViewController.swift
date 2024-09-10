d1ca505717833bf03b808c1baa46dcbf34499313
BoogieManN8 <tengizkiknadze8@gmail.com>
2024-09-10 23:52:34 +0400

    Removed storyboard

---
+++ Netflix Clone/Netflix Clone/MainTabBarViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by constantine Walker on 10.09.24.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        
+    }
+
+
+}
+
