c19883c3cb4df8eaf0603e145155aa9d67203fb0
BoogieManN8 <tengizkiknadze8@gmail.com>
2024-09-11 23:12:00 +0400

    Home View Collection View + HomeView Header

---
+++ Netflix Clone/Netflix Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,16 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by constantine Walker on 11.09.24.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    
+    
+    
+    
+}
